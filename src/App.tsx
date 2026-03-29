e1c22c4e8705fc740a147a1201546742c4cf48ba
Alberto Fernandez <albertofernandezmoya2@.com>
2026-03-30 01:25:41 +0200

    Primer commit: subir proyecto React + Vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,18 @@
+import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import Index from "./pages/Index.tsx";
-import NotFound from "./pages/NotFound.tsx";
+import HeroSection from "./components/HeroSection";
+import MenuSection from "./components/MenuSection";
 
+// Crear un cliente
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <HeroSection />
+      <MenuSection />
+    </QueryClientProvider>
+  );
+}
 
 export default App;
